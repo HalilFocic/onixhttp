945da894235152067b809ea5881a97751b3e1111
Halil Focic <halil.focic@zendev.se>
2025-01-03 09:32:00 +0100

    started with posix implementation since net one was not configurable

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,38 @@
 const std = @import("std");
-const Server = @import("http/server.zig").Server;
-const Request = @import("http/request.zig").Request;
-const Response = @import("http/response.zig").Response;
-pub fn handler(req: *Request, res: *Response) !void {
-    std.debug.print("HNADLER RECEIVED FUNCTION \n", .{});
-    if (std.mem.eql(u8, req.path(), "/")) {
-        try res.text("Hello, World! \n");
-    } else {
-        res.status = .not_found;
-        try res.text("Not Found");
-    }
-}
-
+const posix = std.posix;
+const windows = std.os.windows;
+const builtin = @import("builtin");
+const native_os = builtin.os.tag;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    _ = gpa.allocator();
+    const kernel_backlog = 128;
+    var address = try std.net.Address.resolveIp("127.0.0.1", 8000);
+    const nonblock: u32 = posix.SOCK.NONBLOCK;
+    const sock_flags = posix.SOCK.STREAM | posix.SOCK.CLOEXEC | nonblock;
+    const proto: u32 = if (address.any.family == posix.AF.UNIX) 0 else posix.IPPROTO.TCP;
 
-    var server = try Server.init(allocator, .{
-        .port = 8080,
-        .hostname = "127.0.0.1",
-    });
-    defer server.deinit();
+    const sockfd = try posix.socket(address.any.family, sock_flags, proto);
+    errdefer posix.close(sockfd);
 
-    try server.start(handler);
+    try posix.setsockopt(
+        sockfd,
+        posix.SOL.SOCKET,
+        posix.SO.REUSEADDR,
+        &std.mem.toBytes(@as(c_int, 1)),
+    );
+    switch (native_os) {
+        .windows => {},
+        else => try posix.setsockopt(
+            sockfd,
+            posix.SOL.SOCKET,
+            posix.SO.REUSEPORT,
+            &std.mem.toBytes(@as(c_int, 1)),
+        ),
+    }
+    var socklen = address.getOsSockLen();
+    try posix.bind(sockfd, &address.any, socklen);
+    try posix.listen(sockfd, kernel_backlog);
+    try posix.getsockname(sockfd, &address.any, &socklen);
 }
