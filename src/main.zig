ad0e6eb6d7ca261b46f1ee924b77ae30973314c3
Halil Focic <halil.focic@zendev.se>
2025-01-03 09:46:55 +0100

    add basic infinite accept loop

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const net = std.net;
 const posix = std.posix;
 const windows = std.os.windows;
 const builtin = @import("builtin");
@@ -35,4 +36,35 @@ pub fn main() !void {
     try posix.bind(sockfd, &address.any, socklen);
     try posix.listen(sockfd, kernel_backlog);
     try posix.getsockname(sockfd, &address.any, &socklen);
+    while (true) {
+        var client_address: net.Address = undefined;
+        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+        const socket = posix.accept(sockfd, &client_address.any, &client_address_len, posix.SOCK.CLOEXEC) catch |err| {
+            switch (err) {
+                posix.AcceptError.WouldBlock => {},
+                else => std.debug.print("Error while accepting: {any}\n", .{err}),
+            }
+            continue;
+        };
+        defer posix.close(socket);
+
+        std.debug.print("User connect from address {}", .{client_address});
+        write(socket, "Hello from zig") catch |err| {
+            std.debug.print("error writing: {}\n", .{err});
+        };
+    }
+}
+
+fn write(socket: posix.socket_t, msg: []const u8) !void {
+    const response = try std.fmt.allocPrint(std.heap.page_allocator, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ msg.len, msg });
+    defer std.heap.page_allocator.free(response);
+    var pos: usize = 0;
+    while (pos < response.len) {
+        const written = try posix.write(socket, response[pos..]);
+        if (written == 0) {
+            return error.Closed;
+        }
+        pos += written;
+    }
 }
